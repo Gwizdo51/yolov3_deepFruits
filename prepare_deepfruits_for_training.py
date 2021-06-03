4bae619200c791791273f6108729b631b7bf9123
Gwizdo51 <arthur.clement.51@gmail.com>
2021-06-03 16:50:35 +0200

    change access to files

--- prepare_deepfruits_for_training.py
+++ prepare_deepfruits_for_training.py
@@ -11,19 +11,19 @@ def print_erase(sentence, last_print):
     print(sentence, end="", flush=True)
 
 
-def create_folder_tree(new_dir):
-    os.mkdir(Path(new_dir))
-    os.mkdir(Path(new_dir) / "images")
-    os.mkdir(Path(new_dir) / "images" / "train")
-    os.mkdir(Path(new_dir) / "images" / "test")
-    os.mkdir(Path(new_dir) / "labels")
-    os.mkdir(Path(new_dir) / "labels" / "train")
-    os.mkdir(Path(new_dir) / "labels" / "test")
+def create_folder_tree(new_dir_path):
+    os.mkdir(new_dir_path)
+    os.mkdir(new_dir_path / "images")
+    os.mkdir(new_dir_path / "images" / "train")
+    os.mkdir(new_dir_path / "images" / "test")
+    os.mkdir(new_dir_path / "labels")
+    os.mkdir(new_dir_path / "labels" / "train")
+    os.mkdir(new_dir_path / "labels" / "test")
 
 
-def convert_copy_images(original_dir, new_dir, verbose):
+def convert_copy_images(original_dir_path, new_dir_path, verbose):
 
-    datasets_dir_path = Path(original_dir) / "datasets"
+    datasets_dir_path = original_dir_path / "datasets"
     fruits_folder = next(os.walk(datasets_dir_path))[1]
     last_print = ""
     for fruit_folder in fruits_folder:
@@ -40,7 +40,7 @@ def convert_copy_images(original_dir, new_dir, verbose):
             last_print = jpg_fruit_image
             # convert RGBA to RGB
             rgb_fruit_image = fruit_image.convert('RGB')
-            rgb_fruit_image.save(Path(new_dir) / "images" / "test" / jpg_fruit_image)
+            rgb_fruit_image.save(new_dir_path / "images" / "test" / jpg_fruit_image)
 
         # do the same thing for train images
         images_folder_path = datasets_dir_path / fruit_folder / "TRAIN_RGB"
@@ -54,13 +54,13 @@ def convert_copy_images(original_dir, new_dir, verbose):
             last_print = jpg_fruit_image
             # convert RGBA to RGB
             rgb_fruit_image = fruit_image.convert('RGB')
-            rgb_fruit_image.save(Path(new_dir) / "images" / "train" / jpg_fruit_image)
+            rgb_fruit_image.save(new_dir_path / "images" / "train" / jpg_fruit_image)
 
     # resume normal prints:
     print() if verbose else ...
 
 
-def create_labels(original_dir, new_dir, verbose):
+def create_labels(original_dir_path, new_dir_path, verbose):
 
     # 1: capsicum, 2: rockmelon, 3: apple, 4: avocado, 5: mango, 6: orange, 7: strawberry
     fruits_classes = {
@@ -73,8 +73,8 @@ def create_labels(original_dir, new_dir, verbose):
         'strawberry': "6"
     }
 
-    datasets_dir_path = Path(original_dir) / "datasets"
-    fruits_folder = next(os.walk(Path(datasets_dir_path)))[1]
+    datasets_dir_path = original_dir_path / "datasets"
+    fruits_folder = next(os.walk(datasets_dir_path))[1]
     last_print = ""
     for fruit_folder in fruits_folder:
 
@@ -104,7 +104,7 @@ def create_labels(original_dir, new_dir, verbose):
                         new_label_file_lines_array[line_index][3]
                     ]) + "\n"
                     lines_to_write.append(line_to_write)
-                with open(Path(new_dir) / "labels" / "test" / new_label_file_name, "w") as new_labels_file:
+                with open(new_dir_path / "labels" / "test" / new_label_file_name, "w") as new_labels_file:
                     new_labels_file.writelines(lines_to_write)
 
         # get the train labels
@@ -129,7 +129,7 @@ def create_labels(original_dir, new_dir, verbose):
                             new_label_file_lines_array[line_index][3]
                         ]) + "\n"
                         lines_to_write.append(line_to_write)
-                    with open(Path(new_dir) / "labels" / "train" / new_label_file_name, "w") as new_labels_file:
+                    with open(new_dir_path / "labels" / "train" / new_label_file_name, "w") as new_labels_file:
                         new_labels_file.writelines(lines_to_write)
 
         else:
@@ -152,19 +152,19 @@ def create_labels(original_dir, new_dir, verbose):
                             new_label_file_lines_array[line_index][3]
                         ]) + "\n"
                         lines_to_write.append(line_to_write)
-                    with open(Path(new_dir) / "labels" / "train" / new_label_file_name, "w") as new_labels_file:
+                    with open(new_dir_path / "labels" / "train" / new_label_file_name, "w") as new_labels_file:
                         new_labels_file.writelines(lines_to_write)
     
     # resume normal prints
     print() if verbose else ...
 
 
-def delete_useless_data(new_dir, verbose):
+def delete_useless_data(new_dir_path, verbose):
 
-    images_test_files = [name.split(".")[0] for name in next(os.walk(Path(new_dir) / "images" / "test"))[2]]
-    images_train_files = [name.split(".")[0] for name in next(os.walk(Path(new_dir) / "images" / "train"))[2]]
-    labels_test_files = [name.split(".")[0] for name in next(os.walk(Path(new_dir) / "labels" / "test"))[2]]
-    labels_train_files = [name.split(".")[0] for name in next(os.walk(Path(new_dir) / "labels" / "train"))[2]]
+    images_test_files = [name.split(".")[0] for name in next(os.walk(new_dir_path / "images" / "test"))[2]]
+    images_train_files = [name.split(".")[0] for name in next(os.walk(new_dir_path / "images" / "train"))[2]]
+    labels_test_files = [name.split(".")[0] for name in next(os.walk(new_dir_path / "labels" / "test"))[2]]
+    labels_train_files = [name.split(".")[0] for name in next(os.walk(new_dir_path / "labels" / "train"))[2]]
 
     images_test_files_no_labels = [image_file for image_file in images_test_files if image_file not in labels_test_files]
     images_train_files_no_labels = [image_file for image_file in images_train_files if image_file not in labels_train_files]
@@ -179,25 +179,25 @@ def delete_useless_data(new_dir, verbose):
 
     for image_file in images_test_files_no_labels:
         image_file += ".jpg"
-        os.remove(Path(new_dir) / "images" / "test" / image_file)
+        os.remove(new_dir_path / "images" / "test" / image_file)
     for image_file in images_train_files_no_labels:
         image_file += ".jpg"
-        os.remove(Path(new_dir) / "images" / "train" / image_file)
+        os.remove(new_dir_path / "images" / "train" / image_file)
     for label_file in labels_test_files_no_images:
         label_file += ".txt"
-        os.remove(Path(new_dir) / "labels" / "test" / label_file)
+        os.remove(new_dir_path / "labels" / "test" / label_file)
     for label_file in labels_train_files_no_images:
         label_file += ".txt"
-        os.remove(Path(new_dir) / "labels" / "train" / label_file)
+        os.remove(new_dir_path / "labels" / "train" / label_file)
 
 
-def compute_labels(new_dir, verbose):
+def compute_labels(new_dir_path, verbose):
 
     last_print = ""
     steps = ["test", "train"]
     for step in steps:
     
-        file_names = [name.split(".")[0] for name in next(os.walk(Path(new_dir) / "images" / step))[2]]
+        file_names = [name.split(".")[0] for name in next(os.walk(new_dir_path / "images" / step))[2]]
 
         for file_name in file_names:
 
@@ -205,10 +205,10 @@ def compute_labels(new_dir, verbose):
             last_print = file_name
 
             lines_to_write = []
-            image_file = Image.open(Path(new_dir) / "images" / step / (file_name + ".jpg"))
+            image_file = Image.open(new_dir_path / "images" / step / (file_name + ".jpg"))
             width_px, height_px = image_file.size
 
-            with open(Path(new_dir) / "labels" / step / (file_name + ".txt"), "r") as label_file:
+            with open(new_dir_path / "labels" / step / (file_name + ".txt"), "r") as label_file:
                 for line in label_file.read().split("\n")[:-1]:
 
                     [fruit_class, x1, y1, x2, y2] = [int(item) for item in line.split()]
@@ -226,22 +226,22 @@ def compute_labels(new_dir, verbose):
                     ]) + "\n"
                     lines_to_write.append(line_to_write)
             
-            with open(Path(new_dir) / "labels" / step / (file_name + ".txt"), "w") as label_file:
+            with open(new_dir_path / "labels" / step / (file_name + ".txt"), "w") as label_file:
                 label_file.writelines(lines_to_write)
 
     # resume normal print
     print() if verbose else ...
 
 
-def resize_images(new_dir, max_size, verbose):
+def resize_images(new_dir_path, max_size, verbose):
 
     last_print = ""
     steps = ["test", "train"]
     for step in steps:
 
-        for image_name in next(os.walk(Path(new_dir) / "images" / step))[2]:
+        for image_name in next(os.walk(new_dir_path / "images" / step))[2]:
 
-            image_file = Image.open(Path(new_dir) / "images" / step / image_name)
+            image_file = Image.open(new_dir_path / "images" / step / image_name)
             
             if image_file.width > max_size or image_file.height > max_size:
 
@@ -257,13 +257,13 @@ def resize_images(new_dir, max_size, verbose):
                 else:
                     new_width = max_size
                     new_height = max_size
-                image_file.resize((new_width, new_height)).save(Path(new_dir) / "images" / step / image_name)
+                image_file.resize((new_width, new_height)).save(new_dir_path / "images" / step / image_name)
 
     # resume normal print
     print() if verbose else ...
 
 
-def perpare_dataset(original_dir, new_dir, max_size, verbose):
+def perpare_dataset(original_dir_path, new_dir_path, max_size, verbose):
     """
     Function that transforms the deepFruit dataset into input that Yolov3 can understand,
     and copy the new dataset into the new_dir directory.
@@ -271,34 +271,34 @@ def perpare_dataset(original_dir, new_dir, max_size, verbose):
     
     # create the new directories
     print("Creating folder ...") if verbose else ...
-    create_folder_tree(new_dir)
+    create_folder_tree(new_dir_path)
     print("Done.") if verbose else ...
 
     # convert all images from the original dataset into .jpg and copy them in the new dataset
     print("Converting and copying images ...") if verbose else ...
-    convert_copy_images(original_dir, new_dir, verbose)
+    convert_copy_images(original_dir_path, new_dir_path, verbose)
     print("Done.") if verbose else ...
 
     # create the new labels, but keep x1, y1, x2, y2 for now
     print("Creating labels ...") if verbose else ...
-    create_labels(original_dir, new_dir, verbose)
+    create_labels(original_dir_path, new_dir_path, verbose)
     print("Done.") if verbose else ...
 
     # delete labels without images and images without labels
     print("Deleting useless data ...") if verbose else ...
-    delete_useless_data(new_dir, verbose)
+    delete_useless_data(new_dir_path, verbose)
     print("Done.") if verbose else ...
 
     # get the images height and width, and compute the new values for the label files
     # from (object_class, x1, y1, x2, y2) to (object_class, center_x, center_y, width, height)
     # normalized between 0 and 1
     print("Recomputing labels with image size ...") if verbose else ...
-    compute_labels(new_dir, verbose)
+    compute_labels(new_dir_path, verbose)
     print("Done.") if verbose else ...
 
     # resize each image to 640 pixels maximum
     print("Resizing images ...") if verbose else ...
-    resize_images(new_dir, max_size, verbose)
+    resize_images(new_dir_path, max_size, verbose)
     print("Done.") if verbose else ...
 
     # add a .yaml file
@@ -308,15 +308,16 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_dir', '-i', type=str, default='deepFruits_dataset', help='original deepFruits dataset directory (defaults to "deepruits_dataset")')
-    parser.add_argument('--output_dir', '-o', type=str, default='deepFruits_for_training', help='new directory name for the prepared deepFruits dataset (defaults to "deepFruits_for_training")')
+    parser.add_argument('--output_dir', '-o', type=str, default='./', help='directory in which to store the prepared dataset and the yaml file (defaults to current directory)')
     parser.add_argument('--max_size', type=int, default=640, help='maximum height/width of the output images in pixels (defaults to 640)')
     parser.add_argument('--verbose', '-v', action='store_true', help='print advancement')
 
     args = parser.parse_args()
+    new_dir_path = Path(args.output_dir) / "deepFruits_for_training"
 
-    if os.path.exists(Path(args.output_dir)):
+    if os.path.exists(new_dir_path):
         print("DeepFruits dataset already prepared for training.")
     else:
         print("preparing dataset ...")
-        perpare_dataset(args.input_dir, args.output_dir, args.max_size, args.verbose)
+        perpare_dataset(Path(args.input_dir), new_dir_path, args.max_size, args.verbose)
         print("DeepFruits dataset prepared for training.")
